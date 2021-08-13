d4468463e7f11603c58a3d51e9ecf7abcda507a6
Abdul Haleem <87514566+Halluu1001@users.noreply.github.com>
2021-08-13 10:53:48 +0530

    Update PDF.js

--- PDF.js
+++ PDF.js
@@ -47,16 +47,33 @@ const PDF = (props) => {
     const classes = useStyles();
   return (
    <>
-   <Card className={classes.root}>
+   <Card className={classes.root} >
       <CardContent>
       <div className="Post" ref={ref}>
         <p><img src={Logo} alt= "Logo" width="80px" height="80px"/><h2 style={{fontWeight: 'bold'}}>RUBIX </h2><h2 style={{fontWeight:'400', marginTop:'-70px', marginLeft:'110px'}}>TECHNOLOGIES</h2></p>
         <h5 style={{fontWeight:'400'}}>Rajagiri, 10 Salairoad,</h5>
         <h5 style={{fontWeight:'400', marginBottom: '40px' , marginTop: '-20px'}}>Thillainagar Trichy-620018</h5>
-        <h5 style={{fontWeight:'400', marginBottom: '40px', marginLeft:'400px', marginTop: '-98px'}}>+91 9500601772</h5>
-        <h5 style={{fontWeight:'400', marginBottom: '40px' ,marginLeft:'400px', marginTop: '-30px'}}>www.rubixtek.com</h5>
+        <h5 style={{fontWeight:'400', marginBottom: '40px', marginLeft:'450px', marginTop: '-98px'}}>+91 9500601772</h5>
+        <h5 style={{fontWeight:'400', marginBottom: '40px' ,marginLeft:'450px', marginTop: '-30px'}}>www.rubixtek.com</h5>
         
       <h3>Employee Name:</h3><h3 style={{fontWeight:'500',marginTop: '-58px', marginLeft:'220px'}}>{props.empname}</h3>
+      <TableContainer>
+      <Table className={classes.table} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell align="left">Leaves</StyledTableCell>
+            <StyledTableCell align="right">No.of Days</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+        <StyledTableRow> <StyledTableCell align="left">Approved Leave</StyledTableCell>
+            <StyledTableCell align="right" >{props.aprleave}</StyledTableCell>
+            </StyledTableRow>
+            <StyledTableRow>   <StyledTableCell align="left">Total Leave</StyledTableCell>
+            <StyledTableCell align="right" >{props.leave}</StyledTableCell></StyledTableRow>
+        </TableBody>
+        </Table>
+    </TableContainer><br/>
       <TableContainer>
       <Table className={classes.table} aria-label="customized table">
         <TableHead>
@@ -70,11 +87,7 @@ const PDF = (props) => {
         <StyledTableRow> <StyledTableCell align="left" >Basic Gross Pay</StyledTableCell>
         <StyledTableCell align="right" >{props.basicgross}</StyledTableCell>
         </StyledTableRow>
-            <StyledTableRow> <StyledTableCell align="left">Approved Leave</StyledTableCell>
-            <StyledTableCell align="right" >{props.aprleave}</StyledTableCell>
-            </StyledTableRow>
-            <StyledTableRow>   <StyledTableCell align="left">Total Leave</StyledTableCell>
-            <StyledTableCell align="right" >{props.leave}</StyledTableCell></StyledTableRow>
+           
             <StyledTableRow>  <StyledTableCell align="left">Final Gross Pay</StyledTableCell>
             <StyledTableCell align="right" >{props.fgross}</StyledTableCell></StyledTableRow>
             <StyledTableRow> <StyledTableCell align="left">Basic (40%)</StyledTableCell>
@@ -90,7 +103,7 @@ const PDF = (props) => {
       </CardContent>
     </Card> 
       <Pdf targetRef={ref} filename="Receipt.pdf">
-        {({ toPdf }) => <Button size="small" style={{backgroundColor: '#561571', color: '#FFFFFF', float: 'right',marginTop: -500, marginRight: 50}} onClick={toPdf}>Generate Receipt</Button>}
+        {({ toPdf }) => <Button size="small" style={{backgroundColor: '#561571', color: '#FFFFFF', float: 'right',marginTop: -600, marginRight: 50}} onClick={toPdf}>Generate Receipt</Button>}
       </Pdf>
     </>
   );
