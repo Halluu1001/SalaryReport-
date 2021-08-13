e342843754dfc7087bf5063c14299eb506371e00
Abdul Haleem <87514566+Halluu1001@users.noreply.github.com>
2021-08-13 13:59:56 +0530

    Update PDF.js

--- PDF.js
+++ PDF.js
@@ -8,10 +8,11 @@ import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Logo from './Logo.png';
+// import Up from './Up.PNG';
+// import Down from './Down.PNG';
 
 
 const StyledTableCell = withStyles((theme) => ({
@@ -39,6 +40,7 @@ const StyledTableCell = withStyles((theme) => ({
     },
       root: {
         minWidth: 700,
+        marginRight: -100,
       },
   });
 const ref = React.createRef();
@@ -50,13 +52,17 @@ const PDF = (props) => {
    <Card className={classes.root} >
       <CardContent>
       <div className="Post" ref={ref}>
+      {/* <p><img src={Up} alt= "Up" width="80px" height="80px"/></p> */}
+       
         <p><img src={Logo} alt= "Logo" width="80px" height="80px"/><h2 style={{fontWeight: 'bold'}}>RUBIX </h2><h2 style={{fontWeight:'400', marginTop:'-70px', marginLeft:'110px'}}>TECHNOLOGIES</h2></p>
-        <h5 style={{fontWeight:'400'}}>Rajagiri, 10 Salairoad,</h5>
-        <h5 style={{fontWeight:'400', marginBottom: '40px' , marginTop: '-20px'}}>Thillainagar Trichy-620018</h5>
-        <h5 style={{fontWeight:'400', marginBottom: '40px', marginLeft:'450px', marginTop: '-98px'}}>+91 9500601772</h5>
-        <h5 style={{fontWeight:'400', marginBottom: '40px' ,marginLeft:'450px', marginTop: '-30px'}}>www.rubixtek.com</h5>
-        
+        <h4 style={{fontWeight:'400'}}>Rajagiri, 10 Salairoad,</h4>
+        <h4 style={{fontWeight:'400', marginBottom: '40px' , marginTop: '-20px'}}>Thillainagar Trichy-620018</h4>
+        <h4 style={{fontWeight:'400', marginBottom: '40px', marginLeft:'450px', marginTop: '-98px'}}>+91 9500601772</h4>
+        <h4 style={{fontWeight:'400', marginBottom: '40px' ,marginLeft:'450px', marginTop: '-30px'}}>www.rubixtek.com</h4>
+       
+        <h1 style={{textAlign: "center"}}>PAY SLIP</h1>
       <h3>Employee Name:</h3><h3 style={{fontWeight:'500',marginTop: '-58px', marginLeft:'220px'}}>{props.empname}</h3>
+      <h3>Designation:</h3><h3 style={{fontWeight:'500',marginTop: '-58px', marginLeft:'220px'}}>React JS</h3>
       <TableContainer>
       <Table className={classes.table} aria-label="customized table">
         <TableHead>
@@ -66,11 +72,11 @@ const PDF = (props) => {
           </TableRow>
         </TableHead>
         <TableBody>
-        <StyledTableRow> <StyledTableCell align="left">Approved Leave</StyledTableCell>
-            <StyledTableCell align="right" >{props.aprleave}</StyledTableCell>
-            </StyledTableRow>
             <StyledTableRow>   <StyledTableCell align="left">Total Leave</StyledTableCell>
             <StyledTableCell align="right" >{props.leave}</StyledTableCell></StyledTableRow>
+            <StyledTableRow> <StyledTableCell align="left">Approved Leave</StyledTableCell>
+            <StyledTableCell align="right" >{props.aprleave}</StyledTableCell>
+            </StyledTableRow>
         </TableBody>
         </Table>
     </TableContainer><br/>
@@ -98,12 +104,14 @@ const PDF = (props) => {
         </TableBody>
       </Table>
     </TableContainer>
-    
+    <br/><br/>
+    <h4 style={{ marginBottom: '-54px'}}>Employee Signature</h4><h4 style={{marginLeft: '500px'}}>Director</h4>
+    {/* <p><img src={Down} alt= "Down" width="100px" height="90px" style={{marginTop: '200px'}}/></p> */}
       </div>
       </CardContent>
     </Card> 
       <Pdf targetRef={ref} filename="Receipt.pdf">
-        {({ toPdf }) => <Button size="small" style={{backgroundColor: '#561571', color: '#FFFFFF', float: 'right',marginTop: -600, marginRight: 50}} onClick={toPdf}>Generate Receipt</Button>}
+        {({ toPdf }) => <Button size="small" style={{backgroundColor: '#561571', color: '#FFFFFF', float: 'right',marginTop: -740, marginRight: 50}} onClick={toPdf}>Generate Receipt</Button>}
       </Pdf>
     </>
   );
